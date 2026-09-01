3785f320ae0aa7f3682f5bd1e56fc284e4335fe4
simoneandolfo-ops <simoneandolfo@gmail.com>
2026-09-01 15:00:43 +0200

    RecallShot 0.2.2: bump version

--- recallshot-ci/overrides/app/build.gradle.kts
+++ recallshot-ci/overrides/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.recallshot.app"
         minSdk = 26
         targetSdk = 37
-        versionCode = 3
-        versionName = "0.2.1"
+        versionCode = 4
+        versionName = "0.2.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
