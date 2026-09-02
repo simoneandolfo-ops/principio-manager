68b270dee7c0096a4539faf3a2070433ea8fe197
simoneandolfo-ops <simoneandolfo@gmail.com>
2026-09-03 00:40:42 +0200

    RecallShot 0.2.7: bump safe decoder test version

--- recallshot-ci/overrides/app/build.gradle.kts
+++ recallshot-ci/overrides/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.recallshot.app"
         minSdk = 26
         targetSdk = 37
-        versionCode = 8
-        versionName = "0.2.6"
+        versionCode = 9
+        versionName = "0.2.7"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
