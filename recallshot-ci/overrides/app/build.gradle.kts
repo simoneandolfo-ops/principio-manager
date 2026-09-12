1ec11a1b0a46a3733196c8449c8a5158321fc722
simoneandolfo-ops <simoneandolfo@gmail.com>
2026-09-13 01:32:41 +0200

    Bump RecallShot to 0.3.1 smart classification candidate

--- recallshot-ci/overrides/app/build.gradle.kts
+++ recallshot-ci/overrides/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.recallshot.app"
         minSdk = 26
         targetSdk = 37
-        versionCode = 12
-        versionName = "0.3.0"
+        versionCode = 13
+        versionName = "0.3.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
